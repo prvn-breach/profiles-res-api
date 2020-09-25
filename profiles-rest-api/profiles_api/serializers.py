8e0a907393632601912d7d09cf61537e0d31f200
prvn-breach <praveenking880@gmail.com>
2020-09-26 00:16:54 +0530

    changes

--- profiles-rest-api/profiles_api/serializers.py
+++ profiles-rest-api/profiles_api/serializers.py
@@ -29,3 +29,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
+    
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes the Profile feed Item"""
+    
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = { 'user_profile': { 'read_only': True } }
